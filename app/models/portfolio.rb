c0894a7a041650357de777a32b769557009ce8a2
Linards Berzins <linardsberzins@gmail.com>
2017-11-01 06:19:24 +0000

    implemented custom scopes

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,5 @@
 class Portfolio < ApplicationRecord
 	validates_presence_of :title, :body, :main_image, :thumb_image
+
+	scope :emails, -> { where(subtitle: 'Emails') }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
+  get 'email-items', to: 'portfolios#emails'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about-me', to: 'pages#about'
