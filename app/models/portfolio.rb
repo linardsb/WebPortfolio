a8fbbe72914a81b8d8364d1620817168ca7a5ae7
Linards Berzins <linardsberzins@gmail.com>
2017-11-02 05:56:14 +0000

    implemented default uploads for portfolio feature

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,4 +2,11 @@ class Portfolio < ApplicationRecord
 	validates_presence_of :title, :body, :main_image, :thumb_image
 
 	scope :emails, -> { where(subtitle: 'Emails') }
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= "https://placeholdit.co//i/600x400?&bg=f9e267&fc=000000&text=Portfolio%20item"
+		self.thumb_image ||= "https://placeholdit.co//i/350x200?&bg=99eeff&fc=000000&text=Thumb%20Item"
+	end
 end
