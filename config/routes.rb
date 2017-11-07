7ce10c756422c2cd8727b3edcad874136b5765b9
Linards Berzins <linardsberzins@gmail.com>
2017-11-07 06:26:50 +0000

    implemented basic authentiction

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
   get 'email-items', to: 'portfolios#emails'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
