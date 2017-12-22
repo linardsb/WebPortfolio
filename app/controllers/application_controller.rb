b527de531f022f32cbc8d7b455f923fd70465dd2
Linards Berzins <linardsberzins@gmail.com>
2017-12-22 06:13:54 +0000

    finished pagination and navigation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,21 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+
+  def set_copyright
+  	@copyright = CopyrightTool::Renderer.copyright 'Linards Berzins', 'Copyrighted'
+  end
 end
 
+module CopyrightTool
+	class Renderer
+		def self.copyright name, msg
+			"&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+		end
+	end
+end
 
 
 
