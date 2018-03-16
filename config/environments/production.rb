f72420c3e27d747177b256faa3066d89e19ad9ac
Linards Berzins <linardsberzins@gmail.com>
2018-03-16 05:46:21 +0000

    action cable on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://lb-webportfolio.herokuapp.com', 'http://lb-webportfolio.herokuapp.com']
+  config.action_cable.url = "wss://lb-webportfolio.herokuapp.com/cable"
 end
